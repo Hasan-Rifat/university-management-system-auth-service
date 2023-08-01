a88f8f7fcfa9238eb1b0cd3a21bce152a4fea3ae
Hasan-Rifat <developerhr000@gmail.com>
2023-08-01 18:37:07 +0600

    [AcademicSemester schema model & interfece updated 13.9]

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -3,14 +3,38 @@ import {
   AcademicSemesterModel,
   IAcademicSemester,
 } from './academicSemester.interface';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles,
+} from './academicSemester.constant';
 
 const AcademicSemesterSchema = new Schema<IAcademicSemester>(
   {
-    title: { type: String, required: true },
-    year: { type: Number, required: true },
-    code: { type: String, required: true },
-    startMonth: { type: String, required: true },
-    endMonth: { type: String, required: true },
+    title: {
+      type: String,
+      required: true,
+      enum: academicSemesterTitles,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: academicSemesterCodes,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
   },
   { timestamps: true },
 );
---
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles,
+} from './academicSemester.constant';
+
+// req - validation
+// body-- > object
+// data-- > object
+
+const createAcademicSemesterZodSchema = z.object({
+  body: z.object({
+    title: z.enum([...academicSemesterTitles] as [string, ...string[]], {
+      required_error: 'title is required',
+    }),
+    year: z.number({
+      required_error: 'year is required',
+    }),
+    code: z.enum([...academicSemesterCodes] as [string, ...string[]], {
+      required_error: 'code is required',
+    }),
+    startMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
+      required_error: 'startMonth is required',
+    }),
+    endMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
+      required_error: 'endMonth is required',
+    }),
+  }),
+});
+
+export const AcademicSemesterValidation = {
+  createAcademicSemesterZodSchema,
+};
+
+// await createUserZodSchema.parseAsync(req)
