7563dbeada30a5c776fafa75ca04c12974a668d3
Hasan-Rifat <developerhr000@gmail.com>
2023-08-05 15:11:02 +0600

    [14.15 deperment done]

---
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { AcademicDepartmentController } from './academicDepartment.controller';
+import validateRequest from '../../../middleware/validateRequest';
+import { AcademicDepartmentValidation } from './academicDepartment.validations';
+
+const router = express.Router();
+
+router.post(
+  '/create-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentZodSchema,
+  ),
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.createDepartment,
+);
+
+router.get('/:id', AcademicDepartmentController.getSingleDepartment);
+
+router.get('/', AcademicDepartmentController.getAllDepartments);
+
+router.patch(
+  '/:id',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentZodSchema,
+  ),
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.updateDepartment,
+);
+
+router.delete(
+  '/:id',
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicDepartmentController.deleteDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
